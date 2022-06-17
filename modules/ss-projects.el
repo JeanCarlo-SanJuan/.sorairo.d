b934b6e7d14865b98c04009f55aeb20a4e8edb9d
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-06-17 20:07:40 +0800

    now loads projectile mode immediately since emacs runs as a daemon

--- modules/ss-projects.el
+++ modules/ss-projects.el
@@ -5,24 +5,24 @@
   :config (projectile-mode)
   :bind-keymap
   ("C-c p" . projectile-command-map)
+  
   :init
   (when (file-directory-p "e:/Code")
     (setq projectile-project-search-path '(("e:/Code" . 2) "d:/code")))
   (setq projectile-switch-project-action #'projectile-dired)
-  :defer
-  )
+)
 
 (use-package treemacs
-  :defer
+ :after(projectile)
  :bind(
-("C-c b" . treemacs)
-))
+    ("C-c b" . treemacs)
+ )
+)
 
 (use-package treemacs-projectile
   :after (treemacs projectile)
   :config(treemacs-project-follow-mode)
- )
-
-(with-eval-after-load 'projectile)
-
+)
+ 
+(add-hook 'after-init-hook 'projectile-mode)
 (provide 'ss-projects)
